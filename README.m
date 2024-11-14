4137e3970585fa26171ce2985ef694f376a8b9f9
marina-karpova <149302474+marinatea@users.noreply.github.com>
2024-11-14 19:15:38 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,9 +1,9 @@
-#Aplikacja do generowania artykulow HTML za pomocą AI.
+**Aplikacja do generowania artykulow HTML za pomocą AI.**
 
 Jest to prosta aplikacja Node.js, która pobiera artykuł w formacie tekstowym, generuje jego wersję HTML przy użyciu OpenAI i tworzy stronę podglądu z podstawowym szablonem.
 Aplikacja automatycznie tworzy strony HTML, w tym miejsca na obrazy z tagami `img`, podpisami i nagłówkami w odpowiednich tagach HTML.
 
-# Pliki i foldery:
+Pliki i foldery:
 - /styles - style
 - index.js - główny plik aplikacji
 - config.js - konfiguracja klucza API OpenAI
@@ -12,12 +12,12 @@ Aplikacja automatycznie tworzy strony HTML, w tym miejsca na obrazy z tagami `im
 - artykul.html - wygenerowany OpenAI plik HTML z treścią artykułu
 - podglad.html - strona podglądu z artykułem wstawionym do szablonu.
 
-## Funkcje
+Funkcje
 - Pobiera treść artykułu z podanego URL.
 - Używa modelu GPT OpenAI do generowania struktury HTML z tekstu artykułu.
 - Tworzy podgląd artykułu w spersonalizowanym szablonie HTML.
 
-##Instrukcja uruchomienia:
+Instrukcja uruchomienia:
 
 Przed uruchomieniem aplikacji trzeba zainstalować Node.js.
 
@@ -27,7 +27,7 @@ Przed uruchomieniem aplikacji trzeba zainstalować Node.js.
 npm i
 
 3. Utwarzamy plik .env w głównym katalogu projektu i dodajemy lklucz API:
-## OPENAI_API_KEY=twój_klucz ##
+OPENAI_API_KEY=twój_klucz
 
 4. Uruchamiamy aplikację za pomocą komendy:
 node index.js
